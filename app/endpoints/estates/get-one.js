4bf7f715e196fc32e1f91c1f728ef05b0a194b5d
Marco Paulo <markoskt@gmail.com>
2015-04-19 19:16:49 -0300

    refactor: [GET] Change mongoose to Mongo Native Driver

--- app/endpoints/estates/get-one.js
+++ app/endpoints/estates/get-one.js
@@ -1,34 +1,37 @@
-var model = require('../../models/estates');
+/*------------------------------------*\
+    [ESTATES] GET ONE
+\*------------------------------------*/
 
-/**
- * DELETE ALL OF ESTATES
- */
-var getOneEstate = function(req, reply) {
-	var query = model.find({
-		_id: req.params.ID
-	}).limit(1);
+var getOneEstates = {
+    method: 'GET',
+    path: '/estates/{ESTATEID}',
+    handler: function(req, reply) {
 
-	query.exec(function(err, estate) {
-		if (err) {
-			return reply({
-				'code': 0,
-				'message': 'Something bad happened :(',
-				'description': 'This estate not exist'
-			});
-		}
+        DB 	= req.server.plugins['hapi-mongodb'].db;
+        ObjectID = req.server.plugins['hapi-mongodb'].ObjectID;
 
-		reply(estate);
-	});
+        DB.collection('estates')
+        	.find({
+        		_id: new ObjectID(req.params.ESTATEID)
+        	})
+        	.limit(1)
+        	.toArray(function(err, result) {
+                if (err) {
+                    return reply({
+                        "code": 0,
+                        "message": "Something bad happened :(",
+                        "description": err
+                    });
+                }
+
+                reply(result);
+            });
+    }
 }
+
 
 /**
  * EXPORT FUNCTION
  * @param [server]
  */
-module.exports = function(server) {
-	server.route({
-		method: 'GET',
-		path: '/estates/{ID}',
-		handler: getOneEstate
-	});
-};
+module.exports = getOneEstates;

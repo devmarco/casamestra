a8ef9d4b361751be678633a1bd414644a53f0918
marco paulo patricio <Marco@marcos-MacBook-Pro.local>
2015-05-14 18:50:43 -0300

    refactor: Rewrite the code to use RethinkDB

--- app/endpoints/estates/create.js
+++ app/endpoints/estates/create.js
@@ -1,72 +1,75 @@
-var Valid = require('joi');
-var moment = require('moment');
-var Boom = require('boom');
+var DB 		= require('../../config/settings').db;
+var r 		= require('rethinkdbdash')(DB);
+var Boom 	= require('boom');
+var Joi 	= require('joi');
 
 /*------------------------------------*\
-    [ESTATES] CREATE ONE
+	[ESTATES] CREATE
 \*------------------------------------*/
 
-var createEstate = {
+var createAgent = {
 	method: 'POST',
 	path: '/estates',
 	handler: function(req, reply) {
-		var DB = req.server.plugins['hapi-mongodb'].db,
-			OBJECTID = req.server.plugins['hapi-mongodb'].ObjectID,
-			DATABASE,
-			estate;
 
-		//Set the collection
-		DATABASE = DB.collection('estates');
+		/* First we need check if has an Agent
+		 * If yes, we need check if the Agent exist
+		 */
+		if (req.payload.agent) {
+			r.table('agents')
+				.get(req.payload.agent)	
+				.run()
+				.then(function(result) {
+					if (result) {
+						create();
+					} else {
+						reply(Boom.notFound('Sorry, this agent not exist'));
+					}
+				}).error(function(err) {
+					reply(Boom.forbidden('Try again some time'));
+				});
+		} else {
+			create();
+		}
 
-		//Get the payload
-		estate = req.payload;
+		/*
+		 * Create Estate
+		 */
+		function create() {
 
-		//Checks if this estate was associated with an agent
-		if (estate.agent) {
-
-			if (!OBJECTID.isValid(estate.agent)) {
-				return Boom.notFound('The agent ' + estate.agent + ' not exist');
-			};
-
-			//Search agent
-			DATABASE.findOne({
-				_id: estate.agent
-
-			}, function(err, result) {
-
-				if (err) {
-					return Boom.badRequest(err);
-				}
-
-				createEstate(result);
-			});
-
-		} else createEstate();
-
-		function createEstate(agent) {
-			DATABASE.findOne({
-				'address.lat': req.payload.address.lat,
-				'address.lat': req.payload.address.lng
-
-			}, function(err, result) {
-
-				if (result.length !== 0) {
-					return Boom.badRequest(err);
-
-				} else {
-
-					//Set the update_at field
-					req.payload.created_at = moment().format();
-
-					collection.insertOne(req.payload, function(err, result) {
-						if (err) {
-							return Boom.badRequest(err);
-						}
-
-						reply(estate).code(201);
-					});
-				}
-			});
+			r.table('estates')
+				.filter({
+					location: {
+						lat: req.payload.location.lat,
+						lng: req.payload.location.lng
+					}
+				})
+				.run()
+				.then(function(result) {
+					console.log(result);
+					if (result.length !== 0) {
+						r.table('estates')
+							.insert(req.payload, {
+								conflict: 'error'
+							})
+							.run()
+							.then(function(result) {
+								if (result.errors !== 0) {
+									reply(Boom.conflict('Probably this estate already exist'));
+								} else {
+									reply(req.payload);
+								}
+								
+							}).error(function(err) {
+								reply(Boom.badRequest('Something bad happen :('));
+							});
+					} else {
+						reply(Boom.conflict('Already exist an estate with the same address'));
+					}
+				})
+				.error(function(err) {
+					reply(Boom.badRequest('Try again some time'));
+				});
 		}
 	},
 	config: {
@@ -75,38 +78,38 @@ var createEstate = {
 				abortEarly: false
 			},
 			payload: {
-				title: Valid.string().required(),
-				address: {
-					local: Valid.string().required(),
-					lat: Valid.number().required(),
-					lng: Valid.number().required()
-				},
-				gallery: {
-					cover: Valid.string().uri().required(),
-					photos: Valid.array().items(Valid.string().uri())
-				},
-				features: Valid.array().items(Valid.string()),
-				details: Valid.array(),
-				type: Valid.string().required(),
-				area: Valid.number().required(),
-				bedroom: Valid.number().required(),
-				bathroom: Valid.number().required(),
-				park: Valid.number().required(),
-				price: Valid.number().required(),
-				description: Valid.string().required(),
-				estate_type: Valid.string().required(),
-				exclusive: Valid.boolean().required(),
-				neighborhood: Valid.string().required(),
-				featured: Valid.boolean(),
-				agent: Valid.number(),
-				dogs_allowed: Valid.boolean()
+				title: Joi.string().required(),
+				description: Joi.string().required(),
+				location: Joi.object({
+					lat: Joi.number(),
+					lng: Joi.number()
+				}).required(),
+				address: Joi.string().required(),
+				bathrooms: Joi.number().required(),
+				bedrooms: Joi.number().required(),
+				photos: Joi.object({
+					cover: Joi.string().uri().required(),
+					gallery: Joi.array().items(Joi.string().uri())
+				}).required(),
+				features: Joi.array().items(Joi.string()).required(),
+				details: Joi.object({
+					type: Joi.string(),
+					value: Joi.string()
+				}),
+				homeType: Joi.string().required(),
+				area: Joi.number().required(),
+				garages: Joi.number().required(),
+				price: Joi.number().required(),
+				city: Joi.string().required(),
+				neighborhood: Joi.string().required(),
+				dogAllowed: Joi.boolean(),
+				catAllowed: Joi.boolean(),
+				birdAllowed: Joi.boolean(),
+				exclusive: Joi.boolean(),
+				agent: Joi.number()
 			}
 		}
 	}
 }
 
-/**
- * EXPORT FUNCTION
- * @param [server]
- */
-module.exports = createEstate;
+module.exports = createAgent;

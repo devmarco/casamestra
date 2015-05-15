a065e638a4121a5ca316337c832883e98e1e98d4
Marco Paulo <mpatricio@avenuecode.com>
2015-05-15 18:28:48 -0300

    feature: Add favorite resource

--- app/endpoints/favorites/create.js
+++ app/endpoints/favorites/create.js
@@ -1,55 +1,49 @@
-var estates = require('../../models/estates');
-var users = require('../../models/users');
+var DB 		= require('../../config/settings').db;
+var r 		= require('rethinkdbdash')(DB);
+var Boom 	= require('boom');
+var Joi 	= require('joi');
 
-/**
- * ADD THE FAVORITE
- */
-var createFavorite = function(req, reply) {
-	users.findOne({
-		_id: req.params.USERID
-	}, function(err, user) {
-		if (err) {
-			return reply({
-				"code": 0,
-				"message": "Something bad happened :(",
-				"description": 'This user not exist'
+/*------------------------------------*\
+	[ESTATES] CREATE
+\*------------------------------------*/
+
+var createEstate = {
+	method: 'POST',
+	path: '/estates/{CMID}/favorites',
+	handler: function(req, reply) {
+
+		//[ATENTION] Add userID verification
+
+		r.table('estates')
+			.get(req.params.CMID)
+			.update({ 
+				favorites: r.row("favorites").default([]).append(req.payload.userID) 
+			})
+			.run()
+			.then(function(result) {
+
+				if (result.skipped !== 0) {
+					reply(Boom.notFound('Sorry, this estate not exist'));
+				} else {
+					reply({
+						message: 'Favorite added'
+					});
+				}
+				
+			}).error(function(err) {
+				reply(Boom.badRequest('Try again some time'));
 			});
+	},
+	config: {
+		validate: {
+			options: {
+				abortEarly: false
+			},
+			payload: {
+				userID: Joi.string().required(),
+			}
 		}
-
-		estates.update({
-			_id: req.params.ESTATEID
-		}, {
-			$addToSet: {
-				'favorites.users': user
-			}
-		}, {
-			upsert: true
-		}).exec(function(err, estate) {
-			if (err) {
-				return reply({
-					"code": 0,
-					"message": "Something bad happened :(",
-					"description": 'This estate not exist'
-				});
-			}
-
-			reply({
-				'code': 0,
-				'message': 'success',
-				'description': 'Favorite added'
-			});
-		});
-	});
+	}
 }
 
-/**
- * EXPORT FUNCTION
- * @param [server]
- */
-module.exports = function(server) {
-	server.route({
-		method: 'POST',
-		path: '/estates/{ESTATEID}/favorites/{USERID}',
-		handler: createFavorite
-	});
-}
+module.exports = createEstate;

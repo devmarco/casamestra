00b7b484e04512a43e5aef240fe1ad32057dd2c7
Marco Paulo <markoskt@gmail.com>
2015-05-19 15:13:29 -0300

    Some improvements

--- app/config/routes.js
+++ app/config/routes.js
@@ -3,20 +3,20 @@
  */
 var routes = {
 	agents: {
-		'GET': 		require('../../app/endpoints/agents/get'),
-		'GET-ONE': 	require('../../app/endpoints/agents/get-one'),
-		'POST': 	require('../../app/endpoints/agents/create'),
-		'PUT': 		require('../../app/endpoints/agents/update'),
-		'DELETE': 	require('../../app/endpoints/agents/delete')
+		'GET': 			require('../../app/endpoints/agents/get'),
+		'GET-ONE': 		require('../../app/endpoints/agents/get-one'),
+		'POST': 		require('../../app/endpoints/agents/create'),
+		'PUT': 			require('../../app/endpoints/agents/update'),
+		'DELETE': 		require('../../app/endpoints/agents/delete')
 	},
 	estates: {
-		'GET': 		require('../../app/endpoints/estates/get'),
-		'GET-ONE': 	require('../../app/endpoints/estates/get-one'),
-		'GET-BUY': 	require('../../app/endpoints/estates/get-buy'),
-		'GET-RENT': require('../../app/endpoints/estates/get-rent'),
-		'POST': 	require('../../app/endpoints/estates/create'),
-		'PUT': 		require('../../app/endpoints/estates/update'),
-		'DELETE': 	require('../../app/endpoints/estates/delete'),
+		'GET': 			require('../../app/endpoints/estates/get'),
+		'GET-ONE': 		require('../../app/endpoints/estates/get-one'),
+		'GET-BUY': 		require('../../app/endpoints/estates/get-buy'),
+		'GET-RENT': 	require('../../app/endpoints/estates/get-rent'),
+		'POST': 		require('../../app/endpoints/estates/create'),
+		'PUT': 			require('../../app/endpoints/estates/update'),
+		'DELETE': 		require('../../app/endpoints/estates/delete'),
 	// 	'IMPORT': require('../app/endpoints/estates/import')
 	},
 	// neighborhoods: {
@@ -27,19 +27,19 @@ var routes = {
 	// 	'DELETE': require('../app/endpoints/neighborhoods/delete')
 	// },
 	favorites: {
-	// 	'GET': require('../app/endpoints/favorites/get'),
-	// 	'GET-ESTATE': require('../app/endpoints/favorites/get-user-favorites'),
-	// 	'GET-USER': require('../app/endpoints/favorites/get-users-favorited'),
-		'POST': 	require('../../app/endpoints/favorites/create'),
-	// 	'DELETE': require('../app/endpoints/favorites/delete')
+		'GET': 			require('../../app/endpoints/favorites/get'),
+		'GET-ESTATE': 	require('../../app/endpoints/favorites/get-estates-by-user'),
+		'GET-USER': 	require('../../app/endpoints/favorites/get-users-by-estate'),
+		'POST': 		require('../../app/endpoints/favorites/create'),
+		'DELETE': 		require('../../app/endpoints/favorites/delete')
+	},
+	users: {
+		'GET': 			require('../../app/endpoints/users/get'),
+		'GET-ONE': 		require('../../app/endpoints/users/get-one'),
+		'POST': 		require('../../app/endpoints/users/create'),
+		'PUT': 			require('../../app/endpoints/users/update'),
+		'DELETE': 		require('../../app/endpoints/users/delete')
 	}
-	// users: {
-	// 	'GET': require('../app/endpoints/users/get'),
-	// 	'GET-ONE': require('../app/endpoints/users/get-one'),
-	// 	'POST': require('../app/endpoints/users/create'),
-	// 	'PUT': require('../app/endpoints/users/update'),
-	// 	'DELETE': require('../app/endpoints/users/delete')
-	// }
 }
 
 module.exports = {

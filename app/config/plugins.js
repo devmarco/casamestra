2c9634fe19aea9ff60c2f80b64ea240ee3ee2734
Marco Paulo <markoskt@gmail.com>
2015-06-18 18:11:21 -0300

    chore: remove unused plugins

--- app/config/plugins.js
+++ app/config/plugins.js
@@ -1,26 +1,29 @@
-var plugins = function(server) {
-    var goodOptions = {
-        reporters: [{
-            reporter: require('good-console'),
-            events: { log: '*', response: '*' }
-        }]
-    };
+/*
+ * Good Plugin
+ */
+var goodPlugin = {
+	register: require('good'),
+	options: {
+		reporters: [{
+			reporter: require('good-console'),
+			events: { log: '*', response: '*' }
+		}]
+	}
+}
 
-    /**
-     * Register plugins to hapi server
-     */
-    server.register([{
-        register: require('good'),
-        options: goodOptions
-    }], function(err) {
-        if (err) {
-            throw err;
-        }
-    });
+var plugins = function(server) {
+	/**
+	 * Register plugins to hapi server
+	 */
+	server.register([goodPlugin], function(err) {
+		if (err) {
+			throw err;
+		}
+	});
 };
 
 module.exports = {
-    init: function(server) {
-        plugins(server);
-    }
+	init: function(server) {
+		plugins(server);
+	}
 }

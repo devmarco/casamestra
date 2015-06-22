bb05083acf4b807e27433236a626afea025e0c3d
Marco Paulo <markoskt@gmail.com>
2015-06-22 11:37:07 -0300

    feat: add call to crons

--- app/bin/casamestra.js
+++ app/bin/casamestra.js
@@ -2,6 +2,7 @@ var Hapi 		= require('hapi');
 var settings 	= require('../config/settings');
 var routes 		= require('../config/routes');
 var plugins 	= require('../config/plugins');
+var crons 		= require('../config/crons');
 
 var internals 	= {};
 
@@ -24,8 +25,11 @@ internals.config = (function() {
 	// Bootstrap Hapi Server Plugins, passes the server object to the plugins
 	plugins.init(server);
 	
-	// //Expose routes
+	//Expose routes
 	routes.create(server);
+
+	//Run crons
+	crons.start();
 
 	return {
 		server: server

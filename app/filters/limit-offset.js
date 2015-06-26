e077c3730bfb0555e040cf06319e82e43f48f38a
Marco Paulo <markoskt@gmail.com>
2015-06-26 18:07:57 -0300

    chore: filters code improvements

--- app/filters/limit-offset.js
+++ app/filters/limit-offset.js
@@ -62,24 +62,12 @@ function verifyLimitAndOffset(tableName, req, option) {
 	if (option) customFilter = option;
 
 	if (req.query.limit && req.query.offset) {
-		if (typeof parseInt(req.query.limit) !== 'number' || typeof parseInt(req.query.offset) !== 'number') {
-			return Boom.badRequest('Limit and Offset must be a number');
-		}
-
 		return filterByLimitAndOffset(tableName, req.query.limit, req.query.offset);
 
 	} else if (req.query.offset) {
-		if (typeof parseInt(req.query.offset) !== 'number') {
-			return Boom.badRequest('Offset must be a number');
-		}
-
 		return filterByOffset(tableName, req.query.offset);
 
 	} else if (req.query.limit) {
-		if (typeof parseInt(req.query.limit) !== 'number') {
-			return Boom.badRequest('Limit must be a number');
-		}
-
 		return filterByLimit(tableName, req.query.limit);
 
 	} else {

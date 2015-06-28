0b417a82a192e901c633953aa1900d6400abbbe8
Marco Paulo <markoskt@gmail.com>
2015-06-28 18:52:34 -0300

    feat: add currentPassword to user schema

--- app/models/user.js
+++ app/models/user.js
@@ -12,7 +12,8 @@ var schema = {
 		cellphone: Joi.string().regex(/^(\(11\) [9][0-9]{4}-[0-9]{4})|(\(1[2-9]\) [5-9][0-9]{3}-[0-9]{4})|(\([2-9][1-9]\) [1-9][0-9]{3}-[0-9]{4})$/),
 		homephone: Joi.string().regex(/^(\(11\) [9][0-9]{4}-[0-9]{4})|(\(1[2-9]\) [5-9][0-9]{3}-[0-9]{4})|(\([2-9][1-9]\) [1-9][0-9]{3}-[0-9]{4})$/)
 	}),
-	password: Joi.string()
+	password: Joi.string(),
+	currentPassword: Joi.optional()
 }
 
 module.exports = schema;
